1b9c0bb64f4f5a1cfb2e9b54decbd81006499a33
liujianting <liujianting@qiyi.com>
2018-05-25 22:01:00 +0800

    Initial Commit

---
+++ mycoin/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  mycoin
+//
+//  Created by 刘建廷 on 2018/5/25.
+//  Copyright © 2018年 刘建廷. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ mycoinTests/mycoinTests.m
@@ -0,0 +1,39 @@
+//
+//  mycoinTests.m
+//  mycoinTests
+//
+//  Created by 刘建廷 on 2018/5/25.
+//  Copyright © 2018年 刘建廷. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface mycoinTests : XCTestCase
+
+@end
+
+@implementation mycoinTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
