20e521033394488a757467a1b4dc77cea64b3244
liujianting <liujianting@qiyi.com>
2018-05-30 22:13:35 +0800

    first commit

---
+++ mycoin/TKPriceDifference.h
@@ -0,0 +1,13 @@
+//
+//  TKPriceDifference.h
+//  mycoin
+//
+//  Created by 刘建廷 on 2018/5/27.
+//  Copyright © 2018年 刘建廷. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TKPriceDifference : NSObject
+
+@end
---
+++ mycoin/TKPriceDifference.m
@@ -0,0 +1,13 @@
+//
+//  TKPriceDifference.m
+//  mycoin
+//
+//  Created by 刘建廷 on 2018/5/27.
+//  Copyright © 2018年 刘建廷. All rights reserved.
+//
+
+#import "TKPriceDifference.h"
+
+@implementation TKPriceDifference
+
+@end
